db8ad76732dce178595bdcb1fac2155bba29fd06
oliver <oliver@149334a1-2f27-0410-a3b9-fc62619ac1e6>
2007-01-27 11:21:26 +0000

    nomsg

--- root/usr/lib/libmodcgi.sh
+++ root/usr/lib/libmodcgi.sh
@@ -35,7 +35,6 @@ EOF
 
 cgi_begin() {
 cat << EOF
-Content-type: text/html; charset=iso-8859-1
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
    "http://www.w3.org/TR/html4/loose.dtd">
