929029b0e867903b4fe67a0534de4cba3f255f45
buehmann <buehmann@149334a1-2f27-0410-a3b9-fc62619ac1e6>
2010-03-20 10:01:07 +0000

    Switch URLs:  - reset to defaults (preliminary; needs more work)  - shell access

--- branches/buehmann/url-space/root/usr/lib/libmodfrm.sh
+++ branches/buehmann/url-space/root/usr/lib/libmodfrm.sh
@@ -1,17 +1,17 @@
 frm_begin() {
-    local url
     case $1 in
-	mod) url=/conf ;;
-	*)   url="/packages/$1" ;;
+	mod) _frm_url=/conf ;;
+	*)   _frm_url="/packages/$1" ;;
     esac
-    echo "<form action='/freetz${url}' method='post'>"
+    readonly _frm_url
+    echo "<form action='/freetz${_frm_url}' method='post'>"
 }
 
 frm_end() {
 cat << EOF
 <div class="btn"><input type="submit" value="$(lang de:"&Uuml;bernehmen" en:"Apply")"></div>
 </form>
-<form class="btn" action="/cgi-bin/save.cgi?form=def_$1" method="post">
+<form class="btn" action="/freetz${_frm_url}?form=def_$1" method="post">
 <div class="btn"><input type="submit" value="$(lang de:"Standard" en:"Defaults")"></div>
 </form>
 EOF
