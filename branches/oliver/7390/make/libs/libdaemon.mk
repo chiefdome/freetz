f4b736cba595b7ae57321a039a1f60f350b83aa5
oliver <oliver@149334a1-2f27-0410-a3b9-fc62619ac1e6>
2010-07-29 08:14:54 +0000

    [7390 branch]: * Sync with trunk

---
+++ branches/oliver/7390/make/libs/libdaemon.mk
@@ -0,0 +1,52 @@
+$(call PKG_INIT_LIB, 0.14)
+$(PKG)_LIB_VERSION:=0.5.0
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=509dc27107c21bcd9fbf2f95f5669563
+$(PKG)_SITE:=http://0pointer.de/lennart/projects/libdaemon/
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/libdaemon/.libs/libdaemon.so.$($(PKG)_LIB_VERSION)
+$(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/lib/libdaemon.so.$($(PKG)_LIB_VERSION)
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_LIB)/libdaemon.so.$($(PKG)_LIB_VERSION)
+
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
+$(PKG)_CONFIGURE_ENV += ac_cv_func_setpgrp_void=yes
+
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
+$(PKG)_CONFIGURE_OPTIONS += --enable-shared
+$(PKG)_CONFIGURE_OPTIONS += --disable-lynx
+$(PKG)_CONFIGURE_OPTIONS += --disable-examples
+
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(LIBDAEMON_DIR) all
+
+$($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
+	$(SUBMAKE) -C $(LIBDAEMON_DIR) \
+		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
+		install
+	$(PKG_FIX_LIBTOOL_LA) \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libdaemon.la \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libdaemon.pc
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
+	$(INSTALL_LIBRARY_STRIP)
+
+$(pkg): $($(PKG)_STAGING_BINARY)
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	$(RM) -r \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libdaemon* \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/include/libdaemon \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libdaemon.pc \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/share/doc/libdaemon
+
+$(pkg)-uninstall:
+	$(RM) $(LIBDAEMON_DEST_LIB)/libdaemon*.so*
+
+$(PKG_FINISH)
