83df5a16c0703a9d28289985f841c42310d5233f
oliver <oliver@149334a1-2f27-0410-a3b9-fc62619ac1e6>
2010-09-28 00:32:45 +0000

    [7390] * Update patches for 7320

--- branches/oliver/7390/patches/7320/200-modules_symlink.sh
+++ branches/oliver/7390/patches/7320/200-modules_symlink.sh
@@ -1,2 +1,2 @@
-echo1 "making symlink for modules (-ur8)"
-ln -s 2.6.19.2 ${FILESYSTEM_MOD_DIR}/lib/modules/2.6.19.2-ur8
+echo1 "making symlink for modules (-ar9)"
+ln -s 2.6.28.10 ${FILESYSTEM_MOD_DIR}/lib/modules/2.6.28.10-ar9
