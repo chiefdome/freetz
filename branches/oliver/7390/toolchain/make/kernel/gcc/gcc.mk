b796b242c704ab9edf8566fe31939d0585980f77
oliver <oliver@149334a1-2f27-0410-a3b9-fc62619ac1e6>
2010-10-16 15:53:40 +0000

    ![7390]: * Add binutils-2.20.51.0.11 and gcc-4.4.5 for kernel 2.6.28

--- branches/oliver/7390/toolchain/make/kernel/gcc/gcc.mk
+++ branches/oliver/7390/toolchain/make/kernel/gcc/gcc.mk
@@ -20,11 +20,21 @@
 
 GCC_KERNEL_VERSION:=$(KERNEL_TOOLCHAIN_GCC_VERSION)
 GCC_KERNEL_SOURCE:=gcc-$(GCC_KERNEL_VERSION).tar.bz2
-GCC_KERNEL_MD5:=4a21ac777d4b5617283ce488b808da7b
 GCC_KERNEL_SITE:=@GNU/gcc/gcc-$(GCC_KERNEL_VERSION)
 GCC_KERNEL_DIR:=$(KERNEL_TOOLCHAIN_DIR)/gcc-$(GCC_KERNEL_VERSION)
 GCC_KERNEL_MAKE_DIR:=$(TOOLCHAIN_DIR)/make/kernel/gcc
 GCC_KERNEL_BUILD_DIR:=$(KERNEL_TOOLCHAIN_DIR)/gcc-$(GCC_KERNEL_VERSION)-build
+
+GCC_INITIAL_PREREQ=
+
+ifndef KERNEL_TOOLCHAIN_NO_MPFR
+GCC_DECIMAL_FLOAT:=--disable-decimal-float
+
+GCC_INITIAL_PREREQ+=$(GMP_HOST_BINARY) $(MPFR_HOST_BINARY)
+
+GCC_WITH_HOST_GMP=--with-gmp=$(GMP_HOST_DIR)
+GCC_WITH_HOST_MPFR=--with-mpfr=$(MPFR_HOST_DIR)
+endif
 
 GCC_KERNEL_EXTRA_MAKE_OPTIONS :=
 ifeq ($(strip $(FREETZ_STATIC_TOOLCHAIN)),y)
@@ -44,7 +54,7 @@ $(GCC_KERNEL_DIR)/.unpacked: $(DL_DIR)/$(GCC_KERNEL_SOURCE)
 	done
 	touch $@
 
-$(GCC_KERNEL_BUILD_DIR)/.configured: $(GCC_KERNEL_DIR)/.unpacked
+$(GCC_KERNEL_BUILD_DIR)/.configured: $(GCC_KERNEL_DIR)/.unpacked $(GCC_INITIAL_PREREQ)
 	mkdir -p $(GCC_KERNEL_BUILD_DIR)
 	(cd $(GCC_KERNEL_BUILD_DIR); PATH=$(KERNEL_TOOLCHAIN_PATH) \
 		CC="$(HOSTCC)" \
@@ -55,22 +65,24 @@ $(GCC_KERNEL_BUILD_DIR)/.configured: $(GCC_KERNEL_DIR)/.unpacked
 		--target=$(REAL_GNU_KERNEL_NAME) \
 		--enable-languages=c \
 		--disable-shared \
-		--disable-__cxa_atexit \
-		--enable-target-optspace \
+		--with-newlib \
+		--disable-libssp \
 		--with-gnu-ld \
-		--disable-libmudflap \
+		--with-gnu-as \
 		--without-headers \
 		--disable-threads \
+		$(GCC_WITH_HOST_GMP) \
+		$(GCC_WITH_HOST_MPFR) \
 		--disable-nls \
 	);
 	touch $@
 
 $(GCC_KERNEL_BUILD_DIR)/.compiled: $(GCC_KERNEL_BUILD_DIR)/.configured
-	PATH=$(KERNEL_TOOLCHAIN_PATH) $(MAKE) $(GCC_KERNEL_EXTRA_MAKE_OPTIONS) -C $(GCC_KERNEL_BUILD_DIR) all
+	PATH=$(KERNEL_TOOLCHAIN_PATH) $(MAKE) $(GCC_KERNEL_EXTRA_MAKE_OPTIONS) -C $(GCC_KERNEL_BUILD_DIR) all-gcc
 	touch $@
 
 $(KERNEL_TOOLCHAIN_STAGING_DIR)/bin/$(REAL_GNU_KERNEL_NAME)-gcc: $(GCC_KERNEL_BUILD_DIR)/.compiled
-	PATH=$(KERNEL_TOOLCHAIN_PATH) $(MAKE1) -C $(GCC_KERNEL_BUILD_DIR) install
+	PATH=$(KERNEL_TOOLCHAIN_PATH) $(MAKE1) -C $(GCC_KERNEL_BUILD_DIR) install-gcc
 	$(call GCC_INSTALL_COMMON,$(KERNEL_TOOLCHAIN_STAGING_DIR),$(GCC_KERNEL_VERSION),$(REAL_GNU_KERNEL_NAME),$(HOST_STRIP))
 	$(call REMOVE_DOC_NLS_DIRS,$(KERNEL_TOOLCHAIN_STAGING_DIR))
 
