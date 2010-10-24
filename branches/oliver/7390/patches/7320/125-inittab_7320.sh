5094ebf8100cb1445fb2075d446f5a65478a237e
oliver <oliver@149334a1-2f27-0410-a3b9-fc62619ac1e6>
2010-10-24 14:48:14 +0000

    ![7390]: Fix patches for 7320  * Move rc.mod execution from fwmod to patches/115-rc.S.sh (not used for 7320)  * Use own inittab file (serial console is on ttyS1  * Use AVM's start script groups

--- branches/oliver/7390/patches/7320/125-inittab_7320.sh
+++ branches/oliver/7390/patches/7320/125-inittab_7320.sh
@@ -1,16 +1,12 @@
-# 7320 uses ttyS1 as serial console
-isFreetzType 7320 && return 0
-
 cat << 'EOF' > "${FILESYSTEM_MOD_DIR}/etc/inittab"
 #
 ::restart:/sbin/init
 ::sysinit:/etc/init.d/rc.S
 
 # Start an "askfirst" shell on the console (whatever that may be)
-ttyS0::askfirst:-/bin/sh
+/dev/ttyS1::askfirst:-/bin/sh
 
 # Stuff to do before rebooting
 ::shutdown:/bin/sh -c /var/post_install
-
 
 EOF
