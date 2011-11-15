9a86cd830d711305b63b8e016267da67444cc856
kriegaex <kriegaex@149334a1-2f27-0410-a3b9-fc62619ac1e6>
2011-11-15 12:35:05 +0000

    ttcp package; refs #1494, closes #1203.
    Batch-added automatically by shell script.
    Package status *untested*, needs build, deploy, test.

---
+++ branches/private/kriegaex/sf3978_packages/make/ttcp/ttcp.mk
@@ -0,0 +1,25 @@
+$(call PKG_INIT_BIN, 3.8)
+$(PKG)_BINARY:=$($(PKG)_DIR)/$(pkg)
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)
+
+$(PKG_LOCALSOURCE_PACKAGE)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+	$(SUBMAKE) -C $(TTCP_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(TTCP_DIR) clean
+	 $(RM) $(TTCP_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(TTCP_TARGET_BINARY)
+
+$(PKG_FINISH)
