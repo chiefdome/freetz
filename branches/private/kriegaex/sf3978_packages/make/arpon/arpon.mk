f8bcf1d2181963249d1f662da8a75127c43ab035
kriegaex <kriegaex@149334a1-2f27-0410-a3b9-fc62619ac1e6>
2011-11-15 12:41:33 +0000

    arpon package; refs #1494, closes #1383.
    Batch-added automatically by shell script.
    Package status *untested*, needs build, deploy, test.

---
+++ branches/private/kriegaex/sf3978_packages/make/arpon/arpon.mk
@@ -0,0 +1,37 @@
+$(call PKG_INIT_BIN, 2.6)
+$(PKG)_SOURCE:=ArpON-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=9489883bf40ae9d5f9a3d987a29ab95d
+$(PKG)_SITE:=@SF/$(pkg)
+$(PKG)_DIR:=$(SOURCE_DIR)/ArpON-$($(PKG)_VERSION)
+$(PKG)_BINARIES:=$(pkg)
+$(PKG)_BINARIES_BUILD_DIR:=$($(PKG)_BINARIES:%=$($(PKG)_DIR)/src/%)
+$(PKG)_BINARIES_TARGET_DIR:=$($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/usr/bin/%)
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$(PKG)_DEPENDS_ON := libnet libpcap libdnet
+
+$($(PKG)_BINARIES_BUILD_DIR): $($(PKG)_DIR)/.configured
+	$(SUBMAKE1) -C $(ARPON_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS) -DLINUX -D_GNU_SOURCE \
+		-L$(TARGET_TOOLCHAIN_STAGING_DIR)/lib \
+		-I$(TARGET_TOOLCHAIN_STAGING_DIR)/include"
+
+$($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/src/%
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR)
+
+$(pkg)-clean:
+	-$(SUBMAKE1) -C $(ARPON_DIR) clean
+	$(RM) $(ARPON_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(ARPON_BINARIES_TARGET_DIR)
+
+$(PKG_FINISH)
