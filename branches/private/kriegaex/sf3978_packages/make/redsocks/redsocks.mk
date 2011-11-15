fee0cbde2539fcfb7aaa5c6d6007c2d249445546
kriegaex <kriegaex@149334a1-2f27-0410-a3b9-fc62619ac1e6>
2011-11-15 12:39:01 +0000

    redsocks package; refs #1494, closes #1291.
    Batch-added automatically by shell script.
    Package status *untested*, needs build, deploy, test.

---
+++ branches/private/kriegaex/sf3978_packages/make/redsocks/redsocks.mk
@@ -0,0 +1,34 @@
+$(call PKG_INIT_BIN, 0.1)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+#$(PKG)_SOURCE_MD5:=5485a5834d5b27fe7f8ebe1b6917b9b6
+#$(PKG)_SITE:=https://github.com/darkk/redsocks/tarball/master
+$(PKG)_BINARIES:=$(pkg)
+$(PKG)_BINARIES_BUILD_DIR:=$($(PKG)_BINARIES:%=$($(PKG)_DIR)/%)
+$(PKG)_BINARIES_TARGET_DIR:=$($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/usr/bin/%)
+
+$(PKG)_DEPENDS_ON := libevent
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARIES_BUILD_DIR): $($(PKG)_DIR)/.configured
+	$(SUBMAKE1) -C $(REDSOCKS_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS) -std=gnu99"
+
+$($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/%
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(REDSOCKS_DIR) clean
+	$(RM) $(REDSOCKS_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(REDSOCKS_BINARIES_TARGET_DIR)
+
+$(PKG_FINISH)
