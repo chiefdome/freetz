69d85ced59056e94565f1f08c8f913d86f4d867f
kriegaex <kriegaex@149334a1-2f27-0410-a3b9-fc62619ac1e6>
2011-11-15 12:41:48 +0000

    aeke package; refs #1494, closes #1386.
    Batch-added automatically by shell script.
    Package status *untested*, needs build, deploy, test.

---
+++ branches/private/kriegaex/sf3978_packages/make/aeke/aeke.mk
@@ -0,0 +1,34 @@
+$(call PKG_INIT_BIN, r4)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
+$(PKG)_SOURCE_MD5:=f3bec388102438138234ed6688a7471c
+$(PKG)_SITE:=svn@http://$(pkg).googlecode.com/svn/trunk
+$(PKG)_BINARIES:=$(pkg)d $(pkg) $(pkg)_cp
+$(PKG)_BINARIES_BUILD_DIR:=$($(PKG)_BINARIES:%=$($(PKG)_DIR)/%)
+$(PKG)_BINARIES_TARGET_DIR:=$($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/usr/bin/%)
+
+$(PKG)_DEPENDS_ON := $(STDCXXLIB) openssl zlib
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARIES_BUILD_DIR): $($(PKG)_DIR)/.configured
+	$(SUBMAKE1) -C $(AEKE_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)"
+
+$($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/%
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR)
+
+$(pkg)-clean:
+	-$(SUBMAKE1) -C $(AEKE_DIR) clean
+	$(RM) $(AEKE_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(AEKE_BINARIES_TARGET_DIR)
+
+$(PKG_FINISH)
