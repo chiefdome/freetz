d81a793cb2a4b5b4cf20001ae7d0db503c32b80d
kriegaex <kriegaex@149334a1-2f27-0410-a3b9-fc62619ac1e6>
2011-11-15 12:36:35 +0000

    autossh package; refs #1494, closes #1242.
    Batch-added automatically by shell script.
    Package status *untested*, needs build, deploy, test.

---
+++ branches/private/kriegaex/sf3978_packages/make/autossh/autossh.mk
@@ -0,0 +1,34 @@
+$(call PKG_INIT_BIN, 1.4b)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tgz
+$(PKG)_SITE:=http://www.harding.motd.ca/$(pkg)
+$(PKG)_BINARY:=$($(PKG)_DIR)/$(pkg)
+$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/bin/$(pkg)
+$(PKG)_SOURCE_MD5:=8f9aa006f6f69e912d3c2f504622d6f7
+
+$(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
+$(PKG)_CONFIGURE_OPTIONS += --with-ssh=/usr/bin/ssh
+$(PKG)_LIBS := -lnsl
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_CONFIGURE)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
+		$(SUBMAKE1) -C $(AUTOSSH_DIR) \
+		LIBS="$(AUTOSSH_LIBS)"
+
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(AUTOSSH_DIR) clean
+	$(RM) $(AUTOSSH_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(AUTOSSH_TARGET_BINARY)
+
+$(PKG_FINISH)
