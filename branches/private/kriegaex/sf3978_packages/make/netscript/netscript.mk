aef10c1549c89cdb3d10313dba930cbc2a492df7
kriegaex <kriegaex@149334a1-2f27-0410-a3b9-fc62619ac1e6>
2011-11-15 12:36:06 +0000

    netscript package; refs #1494, closes #1225.
    Batch-added automatically by shell script.
    Package status *untested*, needs build, deploy, test.

---
+++ branches/private/kriegaex/sf3978_packages/make/netscript/netscript.mk
@@ -0,0 +1,33 @@
+$(call PKG_INIT_BIN, 1.7.1)
+$(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tgz
+$(PKG)_SOURCE_MD5:=10c30bd7ec046af98b61f0189ed9418c
+$(PKG)_SITE:=@SF/$(pkg)
+
+$(PKG)_BINARIES:=$(pkg)
+$(PKG)_BINARIES_BUILD_DIR:=$($(PKG)_BINARIES:%=$($(PKG)_DIR)/%)
+$(PKG)_BINARIES_TARGET_DIR:=$($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/usr/bin/%)
+
+$(PKG_SOURCE_DOWNLOAD)
+$(PKG_UNPACKED)
+$(PKG_CONFIGURED_NOP)
+
+$($(PKG)_BINARIES_BUILD_DIR): $($(PKG)_DIR)/.configured
+	$(SUBMAKE1) -C $(NETSCRIPT_DIR) \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)"
+
+$($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/%
+	$(INSTALL_BINARY_STRIP)
+
+$(pkg):
+
+$(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR)
+
+$(pkg)-clean:
+	-$(SUBMAKE) -C $(NETSCRIPT_DIR) clean
+	$(RM) $(NETSCRIPT_DIR)/.configured
+
+$(pkg)-uninstall:
+	$(RM) $(NETSCRIPT_BINARIES_TARGET_DIR)
+
+$(PKG_FINISH)
