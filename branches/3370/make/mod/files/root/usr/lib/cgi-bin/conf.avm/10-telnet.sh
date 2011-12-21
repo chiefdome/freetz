c3e0c6749bf40b4d6b53e4c655f648eefc816e8e
oliver <oliver@149334a1-2f27-0410-a3b9-fc62619ac1e6>
2011-12-21 20:54:32 +0000

    Delete 3370  branch after reintegration

--- branches/3370/make/mod/files/root/usr/lib/cgi-bin/conf.avm/10-telnet.sh
+++
@@ -1,6 +0,0 @@
-sec_begin 'Telnet'
-
-cgi_print_radiogroup_service_starttype \
-    "telnetd" "$AVM_TELNETD" "$(lang de:"Starttyp von telnetd" en:"telnetd start type")" "" 1
-
-sec_end
--- branches/3370/patches/999-remove_run_clock.sh
+++
@@ -1,7 +0,0 @@
-[ "$FREETZ_REMOVE_RUNCLOCK" = "y" ] || return 0;
-
-echo1 "removing run_clock"
-cat > "${FILESYSTEM_MOD_DIR}/bin/run_clock" << EOF
-#!/bin/sh
-:
-EOF
