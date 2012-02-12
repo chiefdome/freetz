47c70173a2cfa1b39d8235b72ff30d174d3574e7
er13 <er13@149334a1-2f27-0410-a3b9-fc62619ac1e6>
2012-02-12 11:39:36 +0000

    [sf3978_packages] apf:  * add missing dependencies  * revise makefile  * refs #1494

--- branches/sf3978_packages/make/apf/apf.mk
+++ branches/sf3978_packages/make/apf/apf.mk
@@ -1,23 +1,26 @@
 $(call PKG_INIT_BIN, 0.8.4)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5:=4411785b05ec59e955152fe4e61705b1
-$(PKG)_SITE:=http://gray-world.net/projects/af/
+$(PKG)_SITE:=http://gray-world.net/projects/af
+
 $(PKG)_BINARIES:=afserver afclient
 $(PKG)_BINARIES_BUILD_DIR:=$($(PKG)_BINARIES:%=$($(PKG)_DIR)/src/%)
 $(PKG)_BINARIES_TARGET_DIR:=$($(PKG)_BINARIES:%=$($(PKG)_DEST_DIR)/usr/bin/%)
 
+$(PKG)_DEPENDS_ON += openssl zlib
+
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
-$(PKG)_LDFLAGS := -lcrypto -lssl -lz
+# touch some files to prevent aclocal.m4 & configure from being regenerated
+$(PKG)_CONFIGURE_PRE_CMDS += touch -t 200001010000.00 configure.ac; touch -r aclocal.m4 config.h.in;
+
+$(PKG)_CONFIGURE_ENV += ac_cv_lib_nsl_gethostbyaddr=no
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARIES_BUILD_DIR): $($(PKG)_DIR)/.configured
-		$(SUBMAKE1) -C $(APF_DIR) \
-		CC="$(TARGET_CC)" \
-		CFLAGS="$(TARGET_CFLAGS)" \
-		LDFLAGS="$(TARGET_LDFLAGS) $(APF_LDFLAGS)"
+	$(SUBMAKE) -C $(APF_DIR)
 
 $($(PKG)_BINARIES_TARGET_DIR): $($(PKG)_DEST_DIR)/usr/bin/%: $($(PKG)_DIR)/src/%
 	$(INSTALL_BINARY_STRIP)
@@ -27,7 +30,7 @@ $(pkg):
 $(pkg)-precompiled: $($(PKG)_BINARIES_TARGET_DIR)
 
 $(pkg)-clean:
-	-$(SUBMAKE1) -C $(APF_DIR) clean
+	-$(SUBMAKE) -C $(APF_DIR) clean
 	$(RM) $(APF_DIR)/.configured
 
 $(pkg)-uninstall:
