411abe7f7ab4284383ba5797268ebf88cde5b4f7
er13 <er13@149334a1-2f27-0410-a3b9-fc62619ac1e6>
2012-02-13 22:09:57 +0000

    [sf3978_packages] libldns:  * fix dependencies, drop libpcap dependency (libdns doesn't depend on it, only one of examples supplied with it)  * various makefile fixes  * refs #1494

--- branches/sf3978_packages/make/libs/ldns/ldns.mk
+++ branches/sf3978_packages/make/libs/ldns/ldns.mk
@@ -2,35 +2,38 @@ $(call PKG_INIT_LIB, 1.6.11)
 $(PKG)_LIB_VERSION:=1.6.11
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SOURCE_MD5:=c55b592a679672281712c457fbb41eb5
-$(PKG)_SITE:=http://nlnetlabs.nl/downloads/$(pkg)/
+$(PKG)_SITE:=http://nlnetlabs.nl/downloads/$(pkg)
+
 $(PKG)_BINARY:=$($(PKG)_DIR)/.libs/lib$(pkg).so.$($(PKG)_LIB_VERSION)
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/lib$(pkg).so.$($(PKG)_LIB_VERSION)
 $(PKG)_TARGET_BINARY:=$($(PKG)_TARGET_DIR)/lib$(pkg).so.$($(PKG)_LIB_VERSION)
 
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
-$(PKG)_DEPENDS_ON := libpcap 
 
+# remove optimization & debug flags
+$(PKG)_CONFIGURE_PRE_CMDS += $(foreach flag,-O[0-9] -g,$(SED) -i -r -e 's,(C(XX)?FLAGS="[^"]*)$(flag)(( [^"]*)?"),\1\3,g' ./configure;)
+
+$(PKG)_CONFIGURE_OPTIONS += --disable-ldns-config
 $(PKG)_CONFIGURE_OPTIONS += --disable-gost
 $(PKG)_CONFIGURE_OPTIONS += --without-pyldns
-$(PKG)_CONFIGURE_OPTIONS += --disable-ldns-config
-$(PKG)_CONFIGURE_OPTIONS += --with-pcap=yes
-
-# Needs openssl >= 1.0.0
-#$(PKG)_CONFIGURE_OPTIONS += --with-ssl="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"  
+$(PKG)_CONFIGURE_OPTIONS += --without-pyldnsx
+$(PKG)_CONFIGURE_OPTIONS += --without-ssl
+# sha2 requires ssl to be enabled
+$(PKG)_CONFIGURE_OPTIONS += --disable-sha2
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE1) -C $(LDNS_DIR)
+	$(SUBMAKE) -C $(LDNS_DIR)
 
 $($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
-	$(SUBMAKE1) -C $(LDNS_DIR) \
+	$(SUBMAKE) -C $(LDNS_DIR) \
 		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
-		install
+		install-h install-lib
 	$(PKG_FIX_LIBTOOL_LA) \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libldns.a
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libldns.la
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
 	$(INSTALL_LIBRARY_STRIP)
@@ -40,12 +43,10 @@ $(pkg): $($(PKG)_STAGING_BINARY)
 $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
 $(pkg)-clean:
-	-$(SUBMAKE1) -C $(LDNS_DIR) clean
+	-$(SUBMAKE) -C $(LDNS_DIR) clean
 	$(RM) -r \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libldns* \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/*ldns.h \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/*ldns \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/share/man/man?/ldns*
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/include/ldns
 
 $(pkg)-uninstall:
 	$(RM) $(LDNS_TARGET_DIR)/libldns*.so*
