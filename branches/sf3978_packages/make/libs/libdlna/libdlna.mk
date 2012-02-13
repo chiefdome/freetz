e28d62e716db4208b71e0b2c72c3cfcd80d84a18
er13 <er13@149334a1-2f27-0410-a3b9-fc62619ac1e6>
2012-02-13 22:47:10 +0000

    [sf3978_packages] libdlna:  * dependency fixes, makefiles fixes  * refs #1494

--- branches/sf3978_packages/make/libs/libdlna/libdlna.mk
+++ branches/sf3978_packages/make/libs/libdlna/libdlna.mk
@@ -2,7 +2,7 @@ $(call PKG_INIT_LIB, 0.2.3)
 $(PKG)_LIB_VERSION:=0.2.3
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_SOURCE_MD5:=2c974f95b711e5fd07f78fc4ebfcca66
-$(PKG)_SITE:=http://$(pkg).geexbox.org/releases/
+$(PKG)_SITE:=http://$(pkg).geexbox.org/releases
 
 $(PKG)_BINARY:=$($(PKG)_DIR)/src/$(pkg).so.$($(PKG)_LIB_VERSION)
 $(PKG)_STAGING_BINARY:=$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/$(pkg).so.$($(PKG)_LIB_VERSION)
@@ -12,12 +12,13 @@ $(PKG)_DEPENDS_ON := ffmpeg
 
 $(PKG)_CONFIGURE_DEFOPTS := n
 $(PKG)_CONFIGURE_OPTIONS += --enable-shared
-$(PKG)_CONFIGURE_OPTIONS += --disable-static
+$(PKG)_CONFIGURE_OPTIONS += --enable-static
 $(PKG)_CONFIGURE_OPTIONS += --disable-debug
 $(PKG)_CONFIGURE_OPTIONS += --disable-optimize
 $(PKG)_CONFIGURE_OPTIONS += --disable-strip
-$(PKG)_CONFIGURE_OPTIONS += --cross-prefix="$(TARGET_CROSS)"
 $(PKG)_CONFIGURE_OPTIONS += --prefix="/usr"
+$(PKG)_CONFIGURE_OPTIONS += --cross-compile
+$(PKG)_CONFIGURE_OPTIONS += --cross-prefix="$(TARGET_CROSS)"
 $(PKG)_CONFIGURE_OPTIONS += --with-ffmpeg-dir="$(TARGET_TOOLCHAIN_STAGING_DIR)/usr"
 
 $(PKG_SOURCE_DOWNLOAD)
@@ -25,12 +26,15 @@ $(PKG_UNPACKED)
 $(PKG_CONFIGURED_CONFIGURE)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(LIBDLNA_DIR)
+	$(SUBMAKE) -C $(LIBDLNA_DIR) \
+		lib
 
 $($(PKG)_STAGING_BINARY): $($(PKG)_BINARY)
 	$(SUBMAKE) -C $(LIBDLNA_DIR) \
 		DESTDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)" \
 		install
+	$(call PKG_FIX_LIBTOOL_LA,prefix) \
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libdlna.pc
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_STAGING_BINARY)
 	$(INSTALL_LIBRARY_STRIP)
@@ -43,8 +47,7 @@ $(pkg)-clean:
 	$(RM) -r \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/libdlna* \
 		$(TARGET_TOOLCHAIN_STAGING_DIR)/include/libdlna \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libdlna.pc \
-		$(TARGET_TOOLCHAIN_STAGING_DIR)/share/doc/libdlna
+		$(TARGET_TOOLCHAIN_STAGING_DIR)/usr/lib/pkgconfig/libdlna.pc
 
 $(pkg)-uninstall:
 	$(RM) $(LIBUPNP_DEST_LIB)/libdlna*.so*
